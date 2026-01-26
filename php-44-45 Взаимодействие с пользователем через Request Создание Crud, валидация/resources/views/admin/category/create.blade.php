91ccde8aded5deb315f5df172a873af94600b95f
Juice2597 <dimkaefimka@gmail.com>
2026-01-26 09:33:11 +0300

    Add files via upload

---
+++ php-44-45 Взаимодействие с пользователем через Request Создание Crud, валидация/resources/views/admin/category/create.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('title', 'Админка создать категорию')
+
+@section('menu')
+    @include("components.admin.menu")
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Создать категорию</div>
+
+                    <div class="card-body">
+
+                        @if(session('success'))
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session('success') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                            </div>
+                        @endif
+
+                        @if(session('error'))
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {{ session('error') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                            </div>
+                        @endif
+
+                        <form method="POST" action="{{ route('admin.categories.store') }}">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <label for="title" class="col-md-4 col-form-label text-md-end">Категория</label>
+
+                                <div class="col-md-6">
+                                    <input id="title" type="text"
+                                           class="form-control @error('name') is-invalid @enderror" name="name"
+                                           value="{{ old('name') }}" autofocus>
+
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+
+
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Создать
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ php-44-45 Взаимодействие с пользователем через Request Создание Crud, валидация/resources/views/components/menu.blade.php
@@ -0,0 +1,12 @@
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">Главная</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('posts.index') ? 'active' : '' }}" href="{{ route('posts.index') }}">Посты</a>
+</li>
+<li class="nav-item {{ request()->routeIs('posts.categories.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('posts.categories.index') }}">Категории</a></li>
+@auth
+    <li class="nav-item">
+        <a class="nav-link {{ request()->routeIs('admin.index') ? 'active' : '' }}" href="{{ route('admin.index') }}">Админка</a>
+    </li>
+@endauth
